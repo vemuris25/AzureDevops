fe8e4d630912784256771b39dab2b57a98f59427
vemuris25 <49367525+vemuris25@users.noreply.github.com>
2019-10-08 14:48:40 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,18 +1,19 @@
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+//import org.springframework.boot.*;
+//import org.springframework.boot.autoconfigure.*;
+//import org.springframework.web.bind.annotation.*;
 
-@RestController
-@EnableAutoConfiguration
+//@RestController
+//@EnableAutoConfiguration
 public class Example {
 
-	@RequestMapping("/")
+//	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Example.class, args);
+//		SpringApplication.run(Example.class, args);
+		System.out.println("Hello World");
 	}
 
 }
